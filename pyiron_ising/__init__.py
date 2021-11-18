8061789b398d2920c385385999ecc6168d59a176
liamhuber <liam.huber@gmail.com>
2021-11-18 09:19:15 +0100

    Expose key classes for import directly

--- pyiron_ising/__init__.py
+++ pyiron_ising/__init__.py
@@ -4,3 +4,5 @@ __version__ = '1.0'
 from pyiron_base import Project
 from pyiron_ising.toolkit import IsingTools
 Project.register_tools('ising', IsingTools)
+from pyiron_ising.model import Model, Chain1D, Square2D, Hex2D, BCC3D, FCC3D
+from pyiron_ising.mutate import Mutation, Flip, Swap, Cluster, Mutator
